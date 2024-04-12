a0b9f2ebec2532cc94226f3eeb2cd1a8a793a427
Tianyu <tiqi@student.unimelb.edu.au>
2024-04-12 10:55:40 +0000

    feat(stream) Add serializeParState and serializeMap
    Add two new functions to the serialization library to serialize

--- libfaasm/faasm/serialization.h
+++ libfaasm/faasm/serialization.h
@@ -4,15 +4,21 @@
 #include <map>
 #include <string>
 #include <vector>
+#include <cstddef> 
+
+// THERE MUST BE SAME TO THE FAABRIC SERIALIZATION !
 namespace faasm {
+
+std::size_t hashVector(const std::vector<uint8_t>& vec);
+
 // Serialiazion and Deserialization from uint8_t vetors to uint32_t
 uint32_t uint8VToUint32(const std::vector<uint8_t>& bytes);
 std::vector<uint8_t> uint32ToUint8V(uint32_t value);
 
 // Serialiazion and Deserialization from uint8_t vetors to Map<string,string>
-std::vector<uint8_t> mapToUint8V(const std::map<std::string, std::string>& map);
-std::map<std::string, std::string> uint8VToMap(
-  const std::vector<uint8_t>& bytes);
+std::vector<uint8_t> serializeMapBinary(const std::map<std::string, std::string>& map);
+std::map<std::string, std::string> deserializeMapBinary(
+  const std::vector<uint8_t>& buffer);
 
 // Serialiazion and Deserialization from uint8_t vetors to Map<string,int>
 std::vector<uint8_t> mapIntToUint8V(const std::map<std::string, int>& map);
@@ -23,5 +29,12 @@ std::vector<uint8_t> serializeFuncState(
   const std::map<std::string, std::vector<uint8_t>>& map);
 std::map<std::string, std::vector<uint8_t>> deserializeFuncState(
   const std::vector<uint8_t>& bytes);
+
+// Serialiazion and Deserialization of Paritioned State Input (same as FuncState)
+std::vector<uint8_t> serializeParState(
+  const std::map<std::string, std::vector<uint8_t>>& map);
+std::map<std::string, std::vector<uint8_t>> deserializeParState(
+  const std::vector<uint8_t>& bytes);
 }
+
 #endif
